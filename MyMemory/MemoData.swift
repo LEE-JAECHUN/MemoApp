d507def918c833acfaf23d271d08cb74b563ff14
jclee <dlwocjs1618@gmail.com>
2019-11-06 06:07:27 +0900

    - MemoDAO에서 삭제,추가 메소드 추가

--- MyMemory/MemoData.swift
+++ MyMemory/MemoData.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class MemoData {
     var momoIdx:Int?        // 데이터 식별값
@@ -14,4 +15,6 @@ class MemoData {
     var contents:String?    // 메모 내용
     var image:UIImage?      // 이미지
     var regdate:Date?       // 작성일
+    // 원본 MemoMO 객체를 참조하기 위한 속성
+    var objectID: NSManagedObjectID?
 }
